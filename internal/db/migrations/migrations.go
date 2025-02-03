5acde4aa2ed434536920d531dffadf1875798dc6
osag1e <osagie6862@gmail.com>
2025-02-03 19:51:02 +0000

    ...

--- internal/db/migrations/migrations.go
+++ internal/db/migrations/migrations.go
@@ -25,24 +25,3 @@ func ApplyMigrations(db *sql.DB) error {
 
 	return nil
 }
-
-func RollbackMigrations(db *sql.DB) error {
-	migrationFiles := []string{
-		"scripts/02_create_users_table.down.sql",
-		"scripts/04_create_sessions_table.down.sql",
-	}
-
-	for _, file := range migrationFiles {
-		migrationSQL, err := os.ReadFile(file)
-		if err != nil {
-			return err
-		}
-
-		_, err = db.Exec(string(migrationSQL))
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
